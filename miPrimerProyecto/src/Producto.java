29a24fd737df5b3d661efb302b4e000d4055048b
jadris7 <aguilaradry7@gmail.com>
2021-07-05 09:34:07 -0500

    se crea primer proyecto de java con visual studio code

---
+++ miPrimerProyecto/src/Producto.java
@@ -0,0 +1,10 @@
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+public class Producto {
+    private String nombre = " ";
+    private float precio = 0.0f;
+    
+    public void mostrar(){
+        System.out.println("El nombre es: "+nombre+", el precio:"+ precio);
+    }
+}
